9705f92dffd6d41d3113da7c4abbbbb274572a83
bool <booleancoercion@gmail.com>
2022-07-11 19:28:59 +0300

    shoved https management to different module

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::{fs::File, io::BufReader};
+mod ssl;
 
 use actix_files::{Files, NamedFile};
 use actix_web::{middleware, web, App, HttpRequest, HttpServer, Responder};
-use rustls::{Certificate, PrivateKey, ServerConfig};
-use rustls_pemfile::{certs, pkcs8_private_keys};
 
 /// simple handle
 async fn index(_: HttpRequest) -> impl Responder {
@@ -27,42 +25,11 @@ async fn main() -> std::io::Result<()> {
         log::info!("starting HTTP server at http://127.0.0.1:80");
         server.bind(("0.0.0.0", 80))?
     } else {
-        let config = load_rustls_config();
+        let config = ssl::load_rustls_config();
 
         log::info!("starting HTTPS server at http://0.0.0.0:443");
         server.bind_rustls(("0.0.0.0", 443), config)?
     }
     .run()
     .await
-}
-
-fn load_rustls_config() -> rustls::ServerConfig {
-    // init server config builder with safe defaults
-    let config = ServerConfig::builder()
-        .with_safe_defaults()
-        .with_no_client_auth();
-
-    // load TLS key/cert files
-    let cert_file = &mut BufReader::new(File::open("ssl/boolco_dev.crt").unwrap());
-    let key_file = &mut BufReader::new(File::open("ssl/boolco_dev.key").unwrap());
-
-    // convert files to key/cert objects
-    let cert_chain = certs(cert_file)
-        .unwrap()
-        .into_iter()
-        .map(Certificate)
-        .collect();
-    let mut keys: Vec<PrivateKey> = pkcs8_private_keys(key_file)
-        .unwrap()
-        .into_iter()
-        .map(PrivateKey)
-        .collect();
-
-    // exit if no keys could be parsed
-    if keys.is_empty() {
-        eprintln!("Could not locate PKCS 8 private keys.");
-        std::process::exit(1);
-    }
-
-    config.with_single_cert(cert_chain, keys.remove(0)).unwrap()
 }
